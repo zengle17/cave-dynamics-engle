8991caaee7569348031ddfc8d1711726a5c7002b
Zach <zengle7@gmail.com>
2017-02-15 19:27:25 -0600

    Files
    added all files

---
+++ 1 project.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 13 15:14:35 2017
+
+@author: Zach
+"""
+# DUE MARCH 1st
+#%%
+project ideas
+
+karst and lava tube modeling and mapping GIS sediments/aquifers
+water/fluid and ionic movement through these environments 
+growth of speleothems and epigenetic deposits (Pb Zn) induced by magmatic
+    movement up after a fault
+aquifer analyses and where to drill
+lava tube growth, mineral crystalization and volcanology
+hazards with these environments (landslides, sinkholes, hazardous minerals)
+resource management (water and minerals and ores) in these env
+how mining consequences/harmful chems can travel through these envionments
+    + through mining shafts, tunnels
+    environmental travel of hazardous tailings, mercury used in gold mining
+    mercury in gw from gold mining... hydraulic mine assessment
+    remediation for mercury methylation
+understand origin and dynamics of water - springs, streams
+how they are affected by warming climate and gw pumping and ET interaction
+what if a river erroded down to a karst layer and dumped into the cave?
+
+# Karst forms from the dissolution of soluble rocks 
+# such as limestone, dolomite, and gypsum.
+# Acidic water breaks these down near cracks or bedding planes
+# As they break down, they become wider allowing more water flow
+# Allowing for exponentially faster formation
+
+# h2o + co2 -> h2co3 carbonic acid
+# caco3 + h2co3 -> Ca + 2hco3-
+
+# in rare cases (lechuguilla cave New Mex)
+# oxygen rich surface waters seep into deep anoxic Karst systems
+# react with sulfides (pyrite or H2S) to form sulfuric acid that diss CaCO3
+# it then goes to precipitate gypsum spleothems 
+H2S	+	2 O2	→	H2SO4	
+    # (sulfide oxidation)
+H2SO4	+	2 H2O	→	SO2−4	+	2 H3O+	
+    # (sulfuric acid dissociation)
+CaCO3	+	2 H3O+	→	Ca2+	+	H2CO3	+	2 H2O	
+    # (calcium carbonate dissolution)
+CaCO3	+	H2SO4	→	CaSO4	+	H2CO3	
+    # (global reaction leading to calcium sulfate)
+CaSO4	+	2 H2O	→	CaSO4 · 2 H2O	
+    # (hydration and gypsum formation)
+
+What affects carbonate precipitation/dissolution... temp, pH, Pressure
+
+# Rare, chemolithoautotrophic bacteria are believed to occur in the cave. 
+# These bacteria feed on the sulfur, iron, 
+# and manganese minerals and may assist in enlarging the cave and determining 
+# the shapes of unusual speleothems. 
+# the majority are sulfur-oxidizing bacteria that utilize primarily atmospheric 
+# oxygen (derived from sunlight-driven photosynthesis) as an electron acceptor.
+#%%
+
+gdal and ogr - can import as python modules do all GIS functions 
+    does all the basic things
+    
+how to set up karst grid - start out pretending cave system is 2D
+land
+
+start out with some incipient karst conduit - focus on erosion
+proportional to discharge 
+Have it in 2d plan view... rain fall goes down into area
+#%%
+import numpy as np # Numerical library
+from matplotlib import pyplot as plt # Plotting library
+import time
+
+# Global constants
+g = 9.8 # [m/s**2]
+rho = 1000 # [kg/m**3] density of water at 293K
+mu = 0.001 # [kg/m*s] dynamic viscosity of water at 293K
+
+def rain_fall():
+
+# *********************************written as module
+# When channel full and P driven flow ***************************************
+def single_channel_v_pdriven(p1, p2, x1, x2, y, w):
+    """
+    what is the velocity as water travels down first crack or bed
+    plane. Input channel width (w) and location in channel (y).
+    pressure gradient (dp/dx) decreases from x start to x end
+    """
+    return (1./(2.*mu))*((p2-p1)/(x2-x1))*((y**2.)-(w*y)) # [m/s]
+    
+def single_channel_vavg_pdriven(p1, p2, x1, x2, w):
+    """
+    what is the avg velocity in a circular laminar channel. max v / 2
+    max v is at center of channel (w/2)
+    """
+    return ((1./(2.*mu))*((p2-p1)/(x2-x1))*(((w/2.)**2.)-(w*(w/2.))))/2. # [m/s]
+
+def single_channel_tau_pdriven(p1, p2, x1, x2, y, w):
+    """
+    what is the shear stress on the cave wall edges or fluid solid contact
+    (y = 0) or in center
+    """
+    return ((p2-p1)/(x2-x1))*(y-(w/2.)) # [Pa]
+
+# When channel not full and g driven flow ***********************************
+def single_channel_Pgradient_gdriven(theta):
+    """
+    what is P gradient driving flow when not full.
+    depends on g driving the dense fluid down a slope (theta)
+    """
+    return rho*g*np.sin(theta * (np.pi/180.)) # [Pa/m]
+
+# *****************************written as class
+
+class conduit_channel_dynamics(object):
+  """
+  single channel circular pipe flow
+  """
+
+  def __init__(self, name='default flow', p1=10, p2=1, x1=0, x2=100, y=1, w=2, theta=1):
+    """
+    Set up
+    """
+    # make these into class variables.
+    self.name = name
+    self.pressurestart = p1
+    self.pressureend = p2
+    self.xstart = x1
+    self.xend = x2
+    self.channellocation = y
+    self.channelwidth = w
+    self.slopedegree = theta
+    # And automatically run the first two functions
+    # with a class, we can run functions that lie below the current point
+    self.single_channel_v_pdriven()
+    self.single_channel_vavg_pdriven()
+    self.single_channel_shear_pdriven()
+    self.single_channel_Pgradient_gdriven()
+    self.single_channel_v_gdriven()
+    self.single_channel_vavg_gdriven()
+
+# When channel full and P driven flow ***************************************
+  def single_channel_v_pdriven(self):
+    """
+    what is the velocity as water travels down first crack or bed
+    plane. Input channel width (w) and location in channel (y).
+    pressure gradient (dp/dx) decreases from x start to x end
+    """
+    self.vp = (1./(2.*mu))*((self.pressureend-self.pressurestart) \ 
+        /(self.xend-self.xstart))*((self.channellocation**2.) \ 
+        -(self.channelwidth*self.channellocation)) # [m/s]
+    
+  def single_channel_vavg_pdriven(self):
+    """
+    what is the avg velocity in a circular laminar channel. max v / 2
+    max v is at center of channel (w/2)
+    """
+    self.vavgp = ((1./(2.*mu))*((self.pressureend-self.pressurestart) \ 
+        /(self.xend-self.xstart))*(((self.channelwidth/2.)**2.) \ 
+        -(self.channelwidth*(self.channelwidth/2.))))/2. # [m/s]
+
+  def single_channel_shear_pdriven(self):
+    """
+    what is the shear stress on the cave wall edges or fluid solid contact
+    (y = 0) or in center
+    """
+    self.taup = ((self.pressureend-self.pressurestart) \ 
+        /(self.xend-self.xstart))*(self.channellocation-(self.channelwidth/2.))
+        # [Pa]
+
+# When channel not full and g driven flow ***********************************
+  def single_channel_Pgradient_gdriven(self):
+    """
+    what is P gradient driving flow when not full.
+    depends on g driving the dense fluid down a slope (theta)
+    """
+    self.pgradg = rho*g*np.sin(self.slopedegree * (np.pi/180.)) # [Pa/m]
+  
+  def single_channel_v_gdriven(self):
+    """
+    what is v of surface flow at a y location when driven by g on a slope
+    """
+    # **** check that i can do self.pgradg
+    # *** does \ symbol allow me to space down the equation?
+    self.vg = (1./(2.*mu))*self.pgradg*((self.channellocation**2.) \ 
+        -(self.channelwidth*self.channellocation)
+
+  def single_channel_vavg_gdriven(self):
+    """
+    what is the avg velocity across surface flow
+    """
+    self.vavgg =
+
+
+
+
+
+
+
+
---
+++ firstLecturesPythonTips.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import sys
+
+counter = 0 
+
+while counter < 50:
+    counter += 1
+    
+if counter == 50:
+    print "50"
+else:
+    print "error"
+#%%  
+    
+# double == is asking a question
+# single = sets a command
+# == same value
+# >  greater than
+# <  less than
+# >= greater than or equal to
+# <= less than or equal to 
+#%%    
+ #loops in python
+#for
+for i in range(7):
+    print i
+#while  \
+i = 0
+while (i<7):
+    print i
+    i += 1
+#%%
+#statements
+#if
+if a == True:
+    print "cat"
+#elif
+elif a == False:
+    print "dog"
+#else
+else:
+    print "??"
+#%%
+
+n_velociraptors = 2
+n_humans = 25
+n_humans - n_velociraptors     #type n then tab to type quickly!
+if n_velociraptors: print "oh no!"
+
+#%%
+n_helpless_humans = 20
+n_human_hunters = 5
+n_velociraptors = 2 
+#if there are more velociraptors than humans, they outflank and devour
+#humans who are not busy holding velociraptors at bay, reproduce
+if n_velociraptors > n_human_hunters:
+    n_helpless_humans = 0
+    n_human_hunters = 0 
+
+else:
+ n_helpless_humans += n_helpless_humans + n_human_hunters - n_velociraptors
+ 
+#%%
+ #each day, each n_velociraptors eats 2 persons
+ #humans trapped for 1 week
+ n_humans = 50
+ n_velociraptors = 2
+#for day in range (7) #creates python list [0,1,2,3,4,5,6]
+ days = range (7)
+ for day in days:
+     n_humans -= (n_velociraptors * 2) #takes humans - 2 people per raptor
+     if n_humans <= 0:  #this loop is within the for loop
+         n_humans = 0   #if so does not print negative amount of humans
+         break     #break means get out of current loop and move on
+print n_humans     #tells how many humans left after 7 days
+#%% 
+    #could set above up as a while loop rather than for loop
+ n_humans = 50
+ n_velociraptors = 2
+ days = range (7)
+while day <7:
+     n_humans -= (n_velociraptors * 2) #takes humans - 2 people per raptor
+     if n_humans <= 0:  
+         n_humans = 0  
+         break     #break means get out of current loop and move on
+print n_humans     #tells how many humans left after 7 days
+#%%
+n_cookies = (14 + 50) / 2
+pie = True
+cookie_monster_is_here = True
+Libby_is_here = False
+
+day = 0
+while cookie_monster_is_here:
+    if n_cookies >= 10:
+        n_cookies -= 10
+    else:
+        n_cookies = 0
+        cookie_monster_is_here = False
+    print day, n_cookies
+    day += 1
+    
+#libby only arrives after cookie monster leaves
+Libby_is_here = True
+pie = False # they have been eaten
+print "pie is gone too :("
+#%%
+import random
+day = 0
+while day<6:
+    n_cupcakes = 30 
+    n_customers = int(10*random.random()) #a random amount of customers 
+    n_cupcakes -= n_customers
+    day += 1
+    if n_cupcakes > 21:
+        print "money"
+    else n_cupcakes > 25:
+        print "50 cent"
+    print day, n_cupcakes
+#%%
+n_humans = input('how many humans are there?\n')
+# this asks the use to enter in (input) the amount of humans in the console
+n_velociraptors = input('how many velociraptors are there?\n')
+#%%
+# and... are both of these true?
+# or... is one of these cases true?
+# is
+a = 5
+b = 5
+a == b #gives true
+a is b #gives false
+#%%
+i = 0
+while True:  #while is a loop... if is a statement
+    if (i >= 5) and (i <= 10): #parentheses are key to order of operations
+        continue #says skip next part and goes back to i=50
+    print i 
+    if i >= 50:
+        break
+#%%
+#simplest infinite loop
+while True:
+    pass
+#%%
+# handling errors with ... try:  .... except:
+try:
+    print asdf
+except: 
+    print "fail!"
+#this one fails.. so you can say try to a bunch of code and except to figure
+    #out where the code chunk is messing up
+try:
+    print "asdf"
+except:
+    print "fail"
+#this one works
+#%%
+    #dictionaries gives you a full list of commands
+d0 = {}
+d['time'] = '5PM'
+d['hunger'] = 'great'
+#%%
+ #functions put something in and gives something out
+def chomp(success):     #define funcion named chomp
+    if success:
+        print "leg lost"
+        dino = happy
+    else:
+        print "whew"
+        dino = sad
+    return dino
+#this made a function
+def colored_dogs(color):
+    try:
+        print "my dog is" + color
+        output_sucess = true
+#%%
+def quadratic(a, b, c):   #to make quadratic equation solver... needs a,b,c
+    try: xpos = x1 = (-b + (((b**2)*(-4*a*c))**0.5)) / (2*a)
+    except: xpos = None  #quad soln only works for certain parabolas
+    try: xneg = x2 = (-b - (((b**2)*(-4*a*c))**0.5)) / (2*a)
+    except: xneg = None
+    return xpos, xneg
+
+solution = quadratic(10, -11, 20) #10x^2 -11x + 20 is pos and above x axis
+print solution
+solution1 = quadratic(10, 11, -30) #now this one is below x axis and pos
+print solution1, "mathematical!"
+#%%
+#modules contain functions and constants...ex: import random... numpy
+import numpy as np  #now numpy can be called for as np rather than numpy
+np.random #brings in the random function from numpy
+classes #can have all the previous and variables
+print "" #this can space out your console output appearance
+#%%
+# numpy is a module that allows us to do all math sort of things
+import numpy as np
+
+a = np.array([[1,2],[3,4]]) # this gives a list of lists
+    # really gives a 2 x 2 matrix that is 1 2 3 4
+print a * a
+
+  # pyton indexes at 0 so have to add one to start at 1
+  # np.arange (0,5) = np.array ([0,1,2,3,4])
+#%%
+from matplotlib import pyplot as plt # Plotting library
+import time
