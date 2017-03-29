8eefea945c222ff9771cf4dbaf7180739d373e7f
zakarias017 <zengle7@gmail.com>
2017-03-29 17:14:28 -0500

    stuff

--- modelingLectures.py
+++ modelingLectures.py
@@ -8,3 +8,21 @@ Created on Wed Mar 22 16:12:29 2017
 Modeling...
 taking a differential equation and making a computer work with it through
 matrix algebra
+test relationships
+predict future or past
+solve diff eqs w/o analytical solns
+link data and model and uncertainties
+you make an assumption that your data is a linear fit or sin fit (sin(time/period))
+    or log or squareroot, etc.j
+    Whats the right functional form?
+    Whats the right parameters to assign?
+        both are uncertainties
+        
+How to model...
+Full realism or simplified rules... use a set of continuum equations
+agent based: agents making decisions... decisions to put in model based on observation
+
+vectors (row and column vectors) are 1D Matrices
+2D matricies (M x N) rows first column second notation
+    diagonal can act as a scalar
+    identity matrix is diagonal full of ones
